38030039876c3bb3fe36d7d44933061887973c74
aadilk96 <aadilanilkumar@gmail.com>
2019-03-09 18:03:45 +0100

    error checks

--- src/rover.ts
+++ src/rover.ts
@@ -11,6 +11,9 @@ export class Rover {
         this.y = y;
         this.direction = direction;
         this.p = new Planet(s, obs);
+        if (this.x > s || this.y > s) {
+          throw Error("coordinates out of bounds");
+        }
     }
 
     getDirection() {
@@ -26,20 +29,27 @@ export class Rover {
       let moveInstruct = instructions.split("");
 
       for (let i in moveInstruct) {
-        switch (i) {
+        switch (moveInstruct[i]) {
           case "L":
+            console.log("before Left:", this.direction);
             let num1 = dirArr.indexOf(this.direction);
-            this.direction = dirArr[(num + 1) % 4];
+            this.direction = dirArr[(num1 + 1) % 4];
+            console.log("after Left:", this.direction);
             break;
           case "R":
+            console.log("before Right:", this.direction);
             let num2 = dirArr.indexOf(this.direction);
-            this.direction = dirArr[(num - 1) % 4];
+            this.direction = dirArr[((num2 - 1) + 4) % 4];
+            console.log("after Right:", this.direction);
             break;
           case "F":
+            console.log("before Forward:", this.direction, this.x, this.y);
             this.x += Number((this.direction === "E")) - Number((this.direction === "W"));
             this.y += Number((this.direction === "N")) - Number((this.direction === "S"));
+            console.log("after Forward:", this.direction, this.x, this.y);
             break;
           case "B":
+            console.log("BACKWARD");
             this.x += Number((this.direction === "E")) + Number((this.direction === "W"));
             this.y += Number((this.direction === "N")) + Number((this.direction === "S"));
             break;
