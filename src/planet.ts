32ff5384311ee11f03a628949627e4edf5a15218
aadilk96 <aadilanilkumar@gmail.com>
2019-03-09 17:28:53 +0100

    +planet && +rover constructors, setters and getters && initial move func

--- src/planet.ts
+++ src/planet.ts
@@ -2,7 +2,15 @@ export class Planet {
     size: number;
     obstacles: [number, number] [];
 
+
     constructor(size: number, obstacles: [number, number] []) {
-        
+        this.size = size;
+        this.obstacles = obstacles;
+    }
+    getSize() {
+      return this.size;
+    }
+    getObstacles() {
+      return this.obstacles;
     }
 }
--- src/rover.ts
+++ src/rover.ts
@@ -7,10 +7,43 @@ export class Rover {
     p: Planet;
 
     constructor(x: number, y: number, direction: string, s: number, obs: [number, number] []) {
-        
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+        this.p = new Planet(s, obs);
+    }
+
+    getDirection() {
+      return this.direction;
+    }
+
+    getCoordinates() {
+      return [this.x, this.y];
     }
 
     move(instructions: string) {
+      let dirArr = ["N", "W", "S", "E"];
+      let moveInstruct = instructions.split("");
 
+      for (let i in moveInstruct) {
+        switch (i) {
+          case "L":
+            let num1 = dirArr.indexOf(this.direction);
+            this.direction = dirArr[(num + 1) % 4];
+            break;
+          case "R":
+            let num2 = dirArr.indexOf(this.direction);
+            this.direction = dirArr[(num - 1) % 4];
+            break;
+          case "F":
+            this.x += Number((this.direction === "E")) - Number((this.direction === "W"));
+            this.y += Number((this.direction === "N")) - Number((this.direction === "S"));
+            break;
+          case "B":
+            this.x += Number((this.direction === "E")) + Number((this.direction === "W"));
+            this.y += Number((this.direction === "N")) + Number((this.direction === "S"));
+            break;
+        }
+      }
     }
 }
